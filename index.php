ec2481422a32481c236483395a24dee814c57889
stml <james@shorttermmemoryloss.com>
2018-03-20 09:42:13 +0200

    breakfast

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+  <title>HOW FAR IS SPACE???</title>
+
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css"
+  integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ=="
+  crossorigin=""/>
+
+  <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js"
+  integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw=="
+  crossorigin=""></script>
+
+  <script type="text/javascript" src="http://maps.stamen.com/js/tile.stamen.js?v1.3.0"></script>
+
+  <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
+
+  <script src="space.js"></script>
+  <link rel="stylesheet" type="text/css" href="space.css">
+
+</head>
+<body>
+   <div id="image"><img src="atmosphere_layers.png" /></div>
+   <div id="text"><p>Click anywhere to recentre. Source: <a href="https://en.wikipedia.org/wiki/K%C3%A1rm%C3%A1n_line">Wikipedia</a>.</p></div>
+   <div id="this_map"></div>
+</body>
+</html>
---
+++ space.js
@@ -0,0 +1,38 @@
+var map;
+var circles;
+
+// Distances in metres
+var troposphere = 20000;
+var stratosphere = 50000;
+var mesosphere = 85000;
+var karman_line = 100000;
+var thermosphere = 690000;
+var exosphere = 10000000;
+
+$( document ).ready(function() {
+  var layer = new L.StamenTileLayer("toner-lite");
+  map = new L.Map("this_map", {
+    center: new L.LatLng(37.98, 23.72),
+    zoom: 5
+  });
+  map.addLayer(layer);
+  circles = new L.LayerGroup();
+  draw(map.getCenter());
+  map.on('click', getCenter);
+});
+
+function getCenter(e) {
+    draw(e.latlng);
+}
+
+function draw(center) {
+    circles.clearLayers();
+    circles.addLayer(L.circle(center, { color: 'LightCoral', fillColor: 'LightCoral', fillOpacity: 0.2, radius: exosphere }));
+    circles.addLayer(L.circle(center, { color: 'lightblue', fillColor: 'lightblue', fillOpacity: 0.2, radius: thermosphere }));
+    circles.addLayer(L.circle(center, { color: 'red', fillColor: 'red', fillOpacity: 0, radius: karman_line }));
+    circles.addLayer(L.circle(center, { color: 'purple', fillColor: 'purple', fillOpacity: 0.2, radius: mesosphere }));
+    circles.addLayer(L.circle(center, { color: 'green', fillColor: 'green', fillOpacity: 0.2, radius: stratosphere }));
+    circles.addLayer(L.circle(center, { color: 'orange', fillColor: 'orange', fillOpacity: 0.2, radius: troposphere }));
+
+    circles.addTo(map);
+}
